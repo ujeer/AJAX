3b3e532e0e4bf4531d5a674a673a4fea23416a30
Rishikesh Paudel <rishikesh0523@gmail.com>
2025-07-23 16:54:33 +0545

    Reconfigured back to localhost

--- frontend/app.js
+++ frontend/app.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", async (e) => {
 
   try {
     // POST to FastAPI
-    const res = await fetch(`https://ajax-demo-xfwj.vercel.app${endpoint}`, {
+    const res = await fetch(`https://localhost:8000${endpoint}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
